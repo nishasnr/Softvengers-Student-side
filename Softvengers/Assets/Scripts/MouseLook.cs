e6bb0d74878f1b6ac1e337f3a3831dd425b521c6
Atul-Acharya-17 <adiatul2001@gmail.com>
2021-03-04 14:57:25 +0800

    Added universe and solar system navigation

---
+++ Softvengers/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MouseLook : MonoBehaviour
+{
+
+    [SerializeField] private float mouseSensitivity = 1000;
+
+    //private float xRotation = 0f;
+
+    [SerializeField] private Transform player;
+
+
+    void FixedUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Softvengers/Assets/Scripts/UniverseRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniverseRotate : MonoBehaviour
+{
+
+    public float speed = 1;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
