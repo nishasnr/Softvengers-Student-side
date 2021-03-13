0fccb0bb18966b89d58ba5721a67cd28de0fb08e
Astha Garg <garg0002@e.ntu.edu.sg>
2021-03-14 01:16:13 +0800

    Added scene change reset pass script

---
+++ Softvengers/Assets/Scripts/ResetPasswordHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetPasswordHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject CurrentPassword;
+    public GameObject NewPassword;
+    public GameObject ReNewPassword;
+    private string currPass;
+    private string newPass;
+    private string reNewPass;
+    public void updatePassword()
+    {
+        currPass = CurrentPassword.GetComponent<InputField>().text;
+        newPass= NewPassword.GetComponent<InputField>().text;
+        reNewPass= ReNewPassword.GetComponent<InputField>().text;
+        if(string.Compare(newPass,reNewPass)==0 && newPass!="" && currPass!="")
+        {
+            Debug.Log("True");
+        }
+        else
+        {
+            Debug.Log("False");
+        }
+
+    }
+}
