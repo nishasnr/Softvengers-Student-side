4611fac51bf12516806bb1a2e43d755d66afd43a
AKSHAT-SHARMA09 <56574585+AKSHAT-SHARMA09@users.noreply.github.com>
2021-03-14 18:14:38 +0800

    UI screens

---
+++ Softvengers/Assets/Scripts/popup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popup : MonoBehaviour
+{
+    public Canvas canvas;
+    public bool a  = false ;
+    public void pop(){
+        if(a == false){
+            a = true;
+            canvas.enabled = true;
+        }else if (a == true){
+            a = false;
+            canvas.enabled = false;
+        }
+    } 
+}
