d01c7681aee44afefa8af85745d51f53629f3b01
Aks <tayalaks2001@gmail.com>
2021-03-21 17:41:57 +0800

    Updated GamePlayController

--- Softvengers/Assets/Scripts/GamePlayController.cs
+++ Softvengers/Assets/Scripts/GamePlayController.cs
@@ -7,7 +7,9 @@ using UnityEngine.UI;
 public class GamePlayController : MonoBehaviour
 {
     public Navigation navigationData;
-    List<Question> questions = new List<Question>();
+    private int questDifficulty;
+
+    List<List<Question>> questions = new List<List<Question>>();
     private int questionNumber = 0;
     public Transform questionName;
     public Transform option1;
@@ -36,64 +38,26 @@ public class GamePlayController : MonoBehaviour
     private bool failedQuestion = false;
     private bool isLocked = false;
 
-<<<<<<< HEAD
-    //Create List of Questions
-=======
+    private int incorrectStreak = 0;
+    private int correctStreak = 0;
+
     List<Question> questionsE = new List<Question>();
     List<Question> questionsM = new List<Question>();
     List<Question> questionsH = new List<Question>();
     private int questionNumberE = 0;
     private int questionNumberM = 0;
     private int questionNumberH = 0;
->>>>>>> ef8f625b94990482b2dbdfd339d602aca0dc6a42
+
     public GamePlayController()
-    {
-        questionNumber = 0;
-        Option o1 = new Option("A", true);
-        Option o2 = new Option("B", false);
-        Option o3 = new Option("C", false);
-        Option o4 = new Option("D", false);
-        List<Option> ol1 = new List<Option>();
-        ol1.Add(o1); ol1.Add(o2); ol1.Add(o3); ol1.Add(o4);
-        Question q1 = new Question("What", ol1);
+    { 
+        initEasyQsts();
+        initMedQsts();
+        initHardQsts();
+        this.questions.Add(questionsE);
+        this.questions.Add(questionsM);
+        this.questions.Add(questionsH);
 
-        Option o5 = new Option("E", false);
-        Option o6 = new Option("F", true);
-        Option o7 = new Option("G", false);
-        Option o8 = new Option("H", false);
-        List<Option> ol2 = new List<Option>();
-        ol2.Add(o5); ol2.Add(o6); ol2.Add(o7); ol2.Add(o8);
-        Question q2 = new Question("Why", ol2);
-
-        Option o9 = new Option("I", false);
-        Option o10 = new Option("J", false);
-        Option o11 = new Option("K", true);
-        Option o12 = new Option("L", false);
-        List<Option> ol3 = new List<Option>();
-        ol3.Add(o9); ol3.Add(o10); ol3.Add(o11); ol3.Add(o12);
-        Question q3 = new Question("How", ol3);
-
-        Option o13 = new Option("M", false);
-        Option o14 = new Option("N", false);
-        Option o15 = new Option("O", false);
-        Option o16 = new Option("P", true);
-        List<Option> ol4 = new List<Option>();
-        ol4.Add(o13); ol4.Add(o14); ol4.Add(o15); ol4.Add(o16);
-        Question q4 = new Question("When", ol4);
-
-        Option o17 = new Option("Q", false);
-        Option o18 = new Option("R", false);
-        Option o19 = new Option("S", false);
-        Option o20 = new Option("T", true);
-        List<Option> ol5 = new List<Option>();
-        ol5.Add(o17); ol5.Add(o18); ol5.Add(o19); ol5.Add(o20);
-        Question q5 = new Question("Where", ol5);
-
-        this.questions.Add(q1);
-        this.questions.Add(q2);
-        this.questions.Add(q3);
-        this.questions.Add(q4);
-        this.questions.Add(q5);
+        questDifficulty = navigationData.planetSelected;
     }
 
 
@@ -442,11 +406,11 @@ public class GamePlayController : MonoBehaviour
         }
 
         failedQuestion = false;
-        questionName.GetComponent<Text>().text = this.questions[questionNumber].questionName;
-        option1.GetComponent<Text>().text = this.questions[questionNumber].options[0].option;
-        option2.GetComponent<Text>().text = this.questions[questionNumber].options[1].option;
-        option3.GetComponent<Text>().text = this.questions[questionNumber].options[2].option;
-        option4.GetComponent<Text>().text = this.questions[questionNumber].options[3].option;
+        questionName.GetComponent<Text>().text = this.questions[questDifficulty][questionNumber].questionName;
+        option1.GetComponent<Text>().text = this.questions[questDifficulty][questionNumber].options[0].option;
+        option2.GetComponent<Text>().text = this.questions[questDifficulty][questionNumber].options[1].option;
+        option3.GetComponent<Text>().text = this.questions[questDifficulty][questionNumber].options[2].option;
+        option4.GetComponent<Text>().text = this.questions[questDifficulty][questionNumber].options[3].option;
         startTime = Time.time;
         currAsteroid = Instantiate(asteroid, new Vector3(0, 0, 15), Random.rotation);
         paused = false;
@@ -458,7 +422,7 @@ public class GamePlayController : MonoBehaviour
             return;
         isLocked = true;
         Shoot shooter = GameObject.FindGameObjectWithTag("Player").GetComponent<Shoot>();
-        List<Option> options = this.questions[questionNumber].options;
+        List<Option> options = this.questions[questDifficulty][questionNumber].options;
 
         if (options[optionNumber].isCorrect == true)
         {
@@ -480,7 +444,7 @@ public class GamePlayController : MonoBehaviour
             Debug.Log("Wrong");
             float score = 0.0f;
             ResultManager.AddRecord(false, score);
-            //DecreaseHealth();
+            DecreaseHealth();
 
             questionNumber++;
             
