2f1a9a3ed41090eb6d7ce227fd0e60d087ce0109
Atul-Acharya-17 <adiatul2001@gmail.com>
2021-03-21 08:21:05 +0800

    Connected Colors

---
+++ Softvengers/Assets/Scripts/AvatarController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image avatarImage;
+    public Player playerData;
+
+    public List<Sprite> characters;
+    void Start()
+    {
+        avatarImage.GetComponent<Image>().sprite = characters[playerData.colorChoice];
+    }
+
+    
+}
