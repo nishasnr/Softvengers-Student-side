06cc004e7d656e89f6ce3eb8599c61f297862752
Aks <tayalaks2001@gmail.com>
2021-03-24 18:44:25 +0800

    SocialMediaController

---
+++ Softvengers/Assets/Scripts/SocialMediaController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialMediaController : MonoBehaviour
+{
+      
+    public void openTwitter()
+    {
+        float sum = 0;
+        for (int i = 0; i < ResultManager.scores.Count; ++i)
+            sum += ResultManager.scores[i];
+
+        string textToDisplay = "I scored " + sum.ToString() + ". Try and beat my score. ";
+        Application.OpenURL("http://twitter.com/intent/tweet?text=" + WWW.EscapeURL(textToDisplay) + "&amp;lang=en");
+    }
+}
